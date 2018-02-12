da57d072611c783a64b6cc13e85c52cef24c4670
dan <danny.vu@mavs.uta.edu>
2018-02-12 16:09:11 -0600

    v2

--- cypher.h
+++ cypher.h
@@ -1,6 +1,7 @@
 #ifndef CYPHER_H
 #define CYPHER_H
 #include <string>
+using namespace std;
 
 class Cypher
 {
@@ -9,12 +10,12 @@ private:
 	int _chars;
 
 public:
-	void Cypher();
+	Cypher();
 	virtual string encrypt(string unencrypted) = 0;
 	virtual string decrypt(string encrypted) = 0;
 	int chars_processed();
 
 protected:
-	chars_processed(int num);
+	int chars_processed(int num);
 };
 #endif
