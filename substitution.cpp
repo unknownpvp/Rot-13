cd57c518498d531a105e4c25b4bc1ac8e28af32e
dan <danny.vu@mavs.uta.edu>
2018-02-12 16:10:33 -0600

    v1

---
+++ substitution.cpp
@@ -0,0 +1,19 @@
+#include "substitution.h"
+
+Substitution::Substitution(string key) : _key{key}, _dekey{key}{}
+
+string Substitution::encrypt(string unencrypted)
+{
+
+
+}
+
+string Substitution::decrypt(string encrypted)
+{
+
+}
+
+string Substitution::substitute(string original, string key)
+{
+
+}
